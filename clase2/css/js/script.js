a642d71a35b3058629347bf2ad4504e64f1a4368
alumno <alumno>
2019-09-03 22:21:13 -0300

    clase3

---
+++ clase2/css/js/script.js
@@ -0,0 +1,19 @@
+
+var btnsaludar;
+
+window.addEventListener('load', function()
+{
+    btnsaludar = document.getElementById('btnsaludar');
+    btnsaludar.addEventListener('click', function(){
+        console.log('Hola como estas');
+    });
+});
+
+
+
+
+
+function saludar()
+{
+    console.log("Hola que tal");
+}
